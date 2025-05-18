c50296b19133ed7e2bfcdb876e90ab96854f7b51
balemansteve <balemansteve@gmail.com>
2025-05-18 19:16:19 -0300

    New implementation

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,30 +11,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head;
-    unsigned int i = 0;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
-    if (*head == NULL)
-        return (-1);
-
-    /* Find node to delete */
-    while (current != NULL && i < index) {
-        current = current->next;
-        i++;
-    }
-
-    if (current == NULL)
-        return (-1);
-
-    /* Update adjacent nodes' pointers */
-    if (current->prev != NULL)
-        current->prev->next = current->next;
-    else
-        *head = current->next;
-
-    if (current->next != NULL)
-        current->next->prev = current->prev;
-
-    free(current);
-    return (1);
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
+	}
+	return (1);
 }
